e6b63dff9ebbc86ba7afdf5019d429012e511c67
paxip <109951080+paxip@users.noreply.github.com>
2022-11-29 14:19:49 +0000

    Installs plotly

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 import pandas as pd
 import numpy as np
+import plotly.express as px
 
 
-arr1 = np.full((3, 3), fill_value = 5)
+df = pd.read_csv("houses_to_rent.csv", index_col=0)
+# Computing the sample average
+area_mean = np.mean(df["area"])
+
+print("The original sample mean of the internal area of houses in Brazil is", area_mean)
+print()
+print("Rest of the dataset:")
+df["area"]
+
+
+
