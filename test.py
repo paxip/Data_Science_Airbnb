24e464fa592a2a34df6fc0d052e63e5668a2e0fc
paxip <109951080+paxip@users.noreply.github.com>
2022-12-01 07:44:39 +0000

    installs matplotlib

--- test.py
+++ test.py
@@ -3,14 +3,35 @@ import numpy as np
 import plotly.express as px
 
 
-df = pd.read_csv("houses_to_rent.csv", index_col=0)
-# Computing the sample average
-area_mean = np.mean(df["area"])
 
-print("The original sample mean of the internal area of houses in Brazil is", area_mean)
-print()
-print("Rest of the dataset:")
-df["area"]
+# df = pd.read_csv("EntriesGender.csv")
+# fig = px.bar(df, x='Discipline', y="Total")
+# fig.show()
+
+# df = pd.read_csv("EntriesGender.csv")
+# fig = px.bar(df, x=("Male", "Female"), y=("Total"))
+# fig.show()
 
 
+
+# df = pd.read_csv("EntriesGender.csv")
+# print(df.head())
+
+# fig = px.bar(df, x = 'Discipline', y = ['Female', 'Male'], color='Total')
+# fig.show()
+
+# df = pd.read_csv('Athletes.csv', encoding= 'unicode_escape')
+# df = pd.read_csv('Athletes.csv', index_col=0)
+# print(df.head())
+
+
+# # Here we use a column with categorical data
+# fig = px.histogram(df, x="NOC")
+# fig.show()
+
+df = pd.read_csv('/Users/apple/Documents/GitHub/Data_Science_Airbnb/Medals.csv', encoding= 'unicode_escape')
+print(df.head())
+long_df = px.data.medals_long()
+fig = px.bar(df, x="Team/NOC", y="Total", color=('Gold', 'Silver', 'Bronze'), title="Long-Form Input")
+fig.show()
 
