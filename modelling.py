45f76148f08e9cc960a33c77e8552257e6156495
paxip <109951080+paxip@users.noreply.github.com>
2023-03-24 15:58:09 +0530

    Creates list of datasets for linear regression model.

--- modelling.py
+++ modelling.py
@@ -8,19 +8,24 @@ import numpy as np
 import pandas as pd
 
 
+
 def splits_dataset(X,y):
     print(f"Number of samples in dataset: {len(X)}")
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-    print("Number of samples in:")
-    print(f"    Training: {len(y_train)}")
-    print(f"    Testing: {len(y_test)}")
-    
     X_test, X_validation, y_test, y_validation = train_test_split(X_test, y_test, test_size=0.5)
-    print("Number of samples in:")
-    print(f"    Training: {len(y_train)}")
-    print(f"    Validation: {len(y_validation)}")
-    print(f"    Testing: {len(y_test)}")
+    
+    data_sets = [X_train, y_train, X_test, y_test, X_validation, y_validation]
+        
+def linear_regression_model(X_train, y_train, X_test, X_validation):
+    model = SGDRegressor()
+    model.fit(X_train, y_train)
+    y_train_pred = model.predict(X_train)
+    y_validation_pred = model.predict(X_validation)
+    y_test_pred = model.predict(X_test)
+
+
+
 
 
 
@@ -52,6 +57,6 @@ def splits_dataset(X,y):
 
 
 if __name__ == '__main__':
-    listing_df = pd.read_csv('/Users/apple/Documents/GitHub/Data_Science_Airbnb/airbnb_datasets/clean_tabular_data.csv')
-    X,y = Data_Preparation.load_airbnb('Price_Night', listing_df)
+    airbnb_df = pd.read_csv('/Users/apple/Documents/GitHub/Data_Science_Airbnb/airbnb_datasets/clean_tabular_data.csv')
+    X,y = Data_Preparation.load_airbnb('Price_Night', airbnb_df)
     print(X)
