e7be21d76780608702ca5fbe709802449f049d12
paxip <109951080+paxip@users.noreply.github.com>
2023-03-24 10:20:07 +0530

    Adds function to split dataset.

--- modelling.py
+++ modelling.py
@@ -1,5 +1,21 @@
-from tabular_data import load_airbnb
+
+from sklearn import datasets
+from sklearn.linear_model import SGDRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 from tabular_data import Data_Preparation
+from tabular_data import load_airbnb
+import numpy as np
+import pandas as pd
+
+def splits_dataset(X,y):
+    print(f"Number of samples in dataset: {len(X)}")
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    print("Number of samples in:")
+    print(f"    Training: {len(y_train)}")
+    print(f"    Testing: {len(y_test)}")
+       
+
 
 
 
