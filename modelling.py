e757dac8d570d70f21588504c876118739d18602
paxip <109951080+paxip@users.noreply.github.com>
2023-03-29 09:53:37 +0530

    Adds function to tune hyperparameters using GridSearchCV.

--- modelling.py
+++ modelling.py
@@ -1,9 +1,10 @@
 
+from sklearn.linear_model import SGDRegressor
 from sklearn import datasets
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
-from sklearn.linear_model import SGDRegressor
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from tabular_data import Data_Preparation
 import itertools
@@ -48,9 +49,9 @@ def custom_tune_regression_model_hyperparameters(model_type, X_train, y_train, X
     RMSE_list = []
 
     for iteration in iteration_dicts:
-            model = model_type(learning_rate=iteration['learning_rate'], max_iter=iteration['max_iter']).fit(X_train, y_train)
-            linear_regression_model(data_sets)
-            #model.fit(X_train, y_train)
+            model = model_type(learning_rate=iteration['learning_rate'], max_iter=iteration['max_iter'], loss=iteration['loss'], fit_intercept=iteration['fit_intercept'], alpha=iteration['alpha'])
+            #linear_regression_model(data_sets)
+            model.fit(X_train, y_train)
             # y_train_pred = model.predict(X_train)
             # train_RMSE = mean_squared_error(y_train, y_train_pred, squared=False)
             y_validation_pred = model.predict(X_validation)
@@ -69,6 +70,24 @@ def custom_tune_regression_model_hyperparameters(model_type, X_train, y_train, X
                 performance_metrics = {'validation RMSE': best_validation_RMSE, 'validation MAE': best_validation_MAE, 'validation R2': best_validation_R2}
     return best_model, performance_metrics, best_iteration_parameters
 
+def tune_regression_model_hyperparametes():
+    parameters = {'loss': ['squared_error', 'huber', 'epsilon_insensitive'], 'alpha': [0.00005,0.0001, 0.0002,], 'max_iter': [500, 1000, 1500]}
+    grid_LR = GridSearchCV(estimator=model, param_grid=parameters, cv=2, refit=True)
+    grid_LR.fit(data_sets[0], data_sets[1])
+    print(" Results from Grid Search " )
+    print("\n The best estimator across ALL searched params:\n",grid_LR.best_estimator_)
+    print("\n The best score across ALL searched params:\n",grid_LR.best_score_)
+    print("\n The best parameters across ALL searched params:\n",grid_LR.best_params_)
+
+    
+    
+
+
+
+
+     
+        
+
 
 
 
@@ -79,23 +98,29 @@ def custom_tune_regression_model_hyperparameters(model_type, X_train, y_train, X
 if __name__ == '__main__':
     airbnb_df = pd.read_csv('/Users/apple/Documents/GitHub/Data_Science_Airbnb/airbnb_datasets/clean_tabular_data.csv')
     X,y = Data_Preparation.load_airbnb('Price_Night', airbnb_df)
-    print(X)
+    # print(X)
     splits_dataset(X,y)
     X_train, y_train, X_test, y_test, X_validation, y_validation = splits_dataset(X,y)
     data_sets = [X_train, y_train, X_test, y_test, X_validation, y_validation]
     model = SGDRegressor()
     
-    linear_regression_model(data_sets)
-    y_train_pred = model.predict(data_sets[0])
-    y_validation_pred = model.predict(data_sets[4])
-    y_test_pred = model.predict(data_sets[2])
+    # linear_regression_model(data_sets)
+    # y_train_pred = model.predict(data_sets[0])
+    # y_validation_pred = model.predict(data_sets[4])
+    # y_test_pred = model.predict(data_sets[2])
     
-    evaluate_regression_model(y_test_pred, y_train_pred, data_sets)
-    grid_dict = {'learning_rate': ['constant', 'optimal', 'invscaling', 'adaptive'], 'max_iter': [500, 1000, 1500, 2000, 2500, 3000], 'loss': ['squared_error', 'huber', 'epsilon_insensitive', 'squared_epsilon_insensitive'], 'fit_intercept' : [True, False], 'alpha': [0.00005,0.0001, 0.00015, 0.0002]}
+    # evaluate_regression_model(y_test_pred, y_train_pred, data_sets)
+    # grid_dict = {'learning_rate': ['constant', 'optimal', 'invscaling', 'adaptive'], 'max_iter': [500, 1000, 1500, 2000, 2500, 3000], 'loss': ['squared_error', 'huber', 'epsilon_insensitive', 'squared_epsilon_insensitive'], 'fit_intercept' : [True, False], 'alpha': [0.00005,0.0001, 0.00015, 0.0002]}
 
 
-    best_model, performance_metrics, best_iteration_parameters = custom_tune_regression_model_hyperparameters(SGDRegressor, X_train, y_train, X_validation, y_validation, grid_dict)
-    print(f"The best model is {best_model}")
-    print(f"The best_parameters are {best_iteration_parameters}")
-    print(f"The best performance_metrics are {performance_metrics}")
+    # best_model, performance_metrics, best_iteration_parameters = custom_tune_regression_model_hyperparameters(SGDRegressor, X_train, y_train, X_validation, y_validation, grid_dict)
+    # print(f"The best model is {best_model}")
+    # print(f"The best_parameters are {best_iteration_parameters}")
+    # print(f"The best performance_metrics are {performance_metrics}")
+
+    tune_regression_model_hyperparametes()
+    
+
+
+
 
