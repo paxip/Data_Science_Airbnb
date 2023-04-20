1c6fa9eb626f6fab13b38d429d5c00c0035d0bc5
paxip <109951080+paxip@users.noreply.github.com>
2023-04-20 16:17:03 +0530

    Creates SGD optimiser and trains model to completion.

--- neural_network.py
+++ neural_network.py
@@ -28,7 +28,7 @@ class AirbnbNightlyPriceRegressionModel(torch.nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.linear_layer = torch.nn.Linear(9, 1)
-        pass
+        
 
     def forward(self, X):
         #  use the layers to process the features.
@@ -42,38 +42,51 @@ if __name__ == '__main__':
     train_set, test_set, validation_set = random_split(dataset, [0.7, 0.15, 0.15])
     batch_size = 4
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    validation_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True)
 
-    batch = next(iter(train_loader))
-
-    # X, y = example
-    # X = X.to(torch.float32)
-    # y = y.to(torch.float32)
+    # batch = next(iter(train_loader))
 
     model = AirbnbNightlyPriceRegressionModel()
     # print(model(X))
 
     def train(model, epochs=10):
+
+        optimiser = torch.optim.SGD(model.parameters(), lr=0.0001, weight_decay=0.001, momentum=0.07)
+
         for epoch in range(epochs):
             for batch in train_loader:
                 X, y = batch
                 X = X.to(torch.float32)
                 y = y.to(torch.float32)
                 y = torch.unsqueeze(y, 1)
-                # # `print(X)` and `print(y)` are used to print the input features and target variable
-                # of a batch respectively. This is done to check if the data is being loaded
-                # correctly into the model.
-                # print(X)
-                # print(y)
-                prediction = model(X)
-                loss = F.mse_loss(prediction, y)
+                y_prediction = model(X)
+                loss = F.mse_loss(y_prediction, y)
                 loss.backward()
-                print(loss)
-            
+                print(f'Train loss is {loss.item()}')
+                optimiser.step()
+                optimiser.zero_grad()
                 
-                break 
+            for batch in validation_loader:
+                X, y = batch
+                X = X.to(torch.float32)
+                y = y.to(torch.float32)
+                y = torch.unsqueeze(y, 1)
+                y_validation = model(X)
+                loss = F.mse_loss(y_validation, y)
+                loss.backward()
+                print(f'Validation loss is {loss.item()}')
+                optimiser.step()
+                optimiser.zero_grad()
+
+            
 
     train(model)
+
+    # Complete the training loop so that it iterates through ever batch.
+    # Optimise model parameters.
+    # Evaluate model performance on the validation dataset after each epoch.
+
 
    
 
